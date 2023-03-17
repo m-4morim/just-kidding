a500de8e18a97287f61edaa5864563524cfc7832
m-4morim <m.4morim@outlook.com>
2023-03-16 21:10:51 -0300

    :tada: Commit inicial

---
+++ kidding.js
@@ -0,0 +1,40 @@
+var n1 = 2
+var n2 = 1
+
+var s = n1 + n2
+
+console.log(s)
+
+let w = 0
+let q = 4
+
+let Soma = w - q
+
+console.log(Soma)
+
+
+var Ab = Soma / 2
+
+console.log(Ab)
+
+var s1 = 5
+var s2 = 10
+var s3 = (s1 + s2) / 2
+
+console.log(s3)
+
+function estCond(s1, s2, s3) {
+    var s3 = 3
+
+    if (s3 >= 7) {
+        console.log("Aprovado")
+    } else if (s3 < 7) {
+        console.log("Recuperação")
+    } else if (s3 >= 5) {
+        console.log("Recuperação")
+    } else {
+        console.log("Reprovado")
+    }
+}
+
+estCond()
